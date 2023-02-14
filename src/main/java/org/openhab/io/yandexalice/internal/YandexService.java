2ce5c8943df417826910f0bd0299cbeca1c88036
Petr Shatsillo <pshatsillo@gmail.com>
2023-02-14 13:26:05 +0300

    Update YandexService.java

--- src/main/java/org/openhab/io/yandexalice/internal/YandexService.java
+++ src/main/java/org/openhab/io/yandexalice/internal/YandexService.java
@@ -17,6 +17,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 import javax.servlet.ServletException;
@@ -64,7 +65,7 @@ import org.slf4j.LoggerFactory;
 public class YandexService implements EventSubscriber {
     protected static final String CONFIG_URI = "io:yandexalice";
     private static final String CFG_TOKEN = "token";
-    private static Logger logger = LoggerFactory.getLogger(YandexService.class);
+    private static final Logger logger = LoggerFactory.getLogger(YandexService.class);
     private final HttpClient httpClient;
     protected static ItemRegistry itemRegistry;
     protected static EventPublisher eventPublisher;
@@ -85,8 +86,8 @@ public class YandexService implements EventSubscriber {
         this.httpClient.setConnectTimeout(30000);
         this.httpClient.setFollowRedirects(false);
 
-        this.itemRegistry = itemRegistry;
-        this.eventPublisher = eventPublisher;
+        YandexService.itemRegistry = itemRegistry;
+        YandexService.eventPublisher = eventPublisher;
     }
 
     private void getInfoFromYandex() {
@@ -196,7 +197,7 @@ public class YandexService implements EventSubscriber {
                                 new JSONObject().put("instance", "on").put("value", stateBool)))));
                 requestObj.put("devices", devices);
                 try (OutputStream os = con.getOutputStream()) {
-                    byte[] input = requestObj.toString().getBytes("UTF-8");
+                    byte[] input = requestObj.toString().getBytes(StandardCharsets.UTF_8);
                     os.write(input, 0, input.length);
                 }
 
